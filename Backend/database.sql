b0c9bbb6d221ad40e35b6aa6259d3e5d6890c2e3
Blackkbeard <rvinesh2011@gmail.com>
2023-09-06 20:31:40 +0800

    Post Enquiry Created

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,3 +1,7 @@
+SELECT column_name, data_type, column_default, is_nullable
+FROM information_schema.columns
+WHERE table_name = 'posts';
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
@@ -13,6 +17,8 @@ CREATE TABLE users (
 ALTER TABLE users 
 ADD COLUMN biography TEXT;
 
+
+
 --insert fake users
 INSERT INTO users (user_id, username, password, email, full_name, is_admin) VALUES('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'test','test1' ,'test1@test.com', 'test', 'false')
 
@@ -21,21 +27,26 @@ CREATE TABLE ride_types (
     type_name VARCHAR(255) NOT NULL
 );
 
+
+
 CREATE TABLE posts (
-    post_id UUID PRIMARY KEY,
-    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
-    ride_type_id UUID REFERENCES ride_types(ride_type_id) ON DELETE SET NULL,
-    location VARCHAR(255) NOT NULL,
-    duration VARCHAR(50) NOT NULL,
-    max_pax INTEGER NOT NULL,
-    details TEXT,
-    ride_date DATE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  post_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+  user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  ride_type_id UUID REFERENCES ride_types(ride_type_id) ON DELETE SET NULL,
+  location VARCHAR(255) NOT NULL,
+  duration INTEGER NOT NULL,
+  max_pax INTEGER NOT NULL,
+  details TEXT,
+  ride_date DATE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (post_id)
 );
+ALTER TABLE posts 
+ADD COLUMN username VARCHAR(255) UNIQUE NOT NULL;
 
 CREATE TABLE post_enquiries (
-    enquiry_id UUID PRIMARY KEY,
+    enquiry_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     post_id UUID REFERENCES posts(post_id) ON DELETE CASCADE,
     enquirer_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     message TEXT NOT NULL,
@@ -43,7 +54,7 @@ CREATE TABLE post_enquiries (
 );
 
 CREATE TABLE rider_reviews (
-    review_id UUID PRIMARY KEY,
+    review_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     reviewer_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     reviewed_user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     points INTEGER NOT NULL CHECK (points IN (-1, 1)),
@@ -52,7 +63,7 @@ CREATE TABLE rider_reviews (
 );
 
 CREATE TABLE post_registrations (
-    registration_id UUID PRIMARY KEY,
+    registration_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     post_id UUID REFERENCES posts(post_id) ON DELETE CASCADE,
     user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
